3c0c86417a849b83d753ddfa5bbccbb988adc898
孤叶 <123366376+SolitaryBlade@users.noreply.github.com>
2026-07-22 13:24:19 +0800

    Create blocks.js

---
+++ scripts/blocks.js
@@ -0,0 +1,9 @@
+const lib = require("将至lib")
+//地板方块文件
+const 铜矿石 =extend(OreBlock,"Ore-铜",{});
+const 钯金矿 =extend(OreBlock,"Ore-钯金",{});
+const 钴矿 =extend(OreBlock,"Ore-钴",{});
+const 金刚石矿 =extend(OreBlock,"Ore-金刚石",{});
+const 原铁 =extend(OreBlock,"Ore-铁矿石",{});
+const 铀矿 =extend(OreBlock,"Ore-铀",{});
+const 陨铁矿 =extend(OreBlock,"Ore-陨铁",{});
